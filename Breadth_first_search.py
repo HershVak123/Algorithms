f0dced812bde69181d650504aa03f54cea8e8d29
hersh.vakharia <Blahbutte13>
2019-10-16 22:46:26 -0400

    added annotations to second BFFS function

--- Breadth_first_search.py
+++ Breadth_first_search.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
+relation_graph = {'A': ['B', 'C', 'E'],
+                  'B': ['A', 'D', 'E'],
+                  'C': ['A', 'F', 'G'],
+                  'D': ['B'],
+                  'E': ['A', 'B', 'D'],
+                  'F': ['C'],
+                  'G': ['C']}
+
+
 class Bfs:
 
-    def __init__(self, graph ):
+    def __init__(self, graph):
         self.graph = graph
         self.shortest_path = None
         self.connected_nodes = None
@@ -52,30 +61,50 @@ class Bfs:
         :return: Returns a list containing the shortest path between the start and end point
         """
 
+        # Initialize our explored list
         explored = []
 
+        # Initialize our queue (since our queue will contain paths, each element of the list will be
+        # a list in itself
         queue = [[start]]
 
+        # Start with the simplest case
         if start == end:
             return "These two points are adjacent."
 
+        # Iterate over the queue until its empty
         while queue:
 
+            # Take the first list out of the queue
             path = queue.pop(0)
 
+            # Take the last element of the list
             node = path[-1]
 
+            # If that element isn't in explored, add look at the neighbors of the node
             if node not in explored:
                 neighbors = self.graph[node]
 
+                # Iterate over each neighbor and and append the node to the path list
                 for neighbor in neighbors:
                     new_path = list(path)
                     new_path.append(neighbor)
                     queue.append(new_path)
 
+                    # If the neighboring node is the end node, then we have found the shortest path
                     if neighbor == end:
                         return new_path
 
+                # If none of the neighboring nodes are the end node, append the current node in queue to explored list
+                # and begin next iteration
                 explored.append(node)
 
+        # If the queue empties and we do not have a shortest path to return, then the two nodes must not be connected
         return "These two nodes are not connected."
+
+
+if __name__ == '__main__':
+    bfs = Bfs(relation_graph)
+    print(bfs.bfs_connected('B'))
+    print(bfs.bfs_shortest('A', 'G'))
+
